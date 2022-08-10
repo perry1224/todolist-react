34283ed171d905bf1b6a960e72ce744ab834ddec
perry1224 <perry.w1224@gmail.com>
2022-08-10 13:36:43 -0400

    update todo

--- todolist-react/src/App.js
+++ todolist-react/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from 'react';
+import Todolist from './Todolist';
+import { v4 as uuidv4 } from 'uuid'
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+function handleAddtodo(e) {
+const name = todoNameRef.current.value
+if (name === '') return 
+setTodos(prevTodos => {
+  return [...prevTodos, {id: uuidv4(), name: name, complete: false}]
+})
+todoNameRef.current.value = null
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Todolist todos = {todos}/>
+    <input ref={todoNameRef} type = "text" />
+    <button onClick={handleAddtodo}> Add Todos</button>
+    <button>Clear Complete</button>
+    <div>0 left to do</div>
+    </>
   );
 }
 
